68681279e96cd010719d7920820d3e8139077246
Davis Marques <dmarques@freshbits.io>
2017-11-22 23:29:06 +1100

    Add cask definition

---
+++ Casks/doc.rb
@@ -0,0 +1,11 @@
+cask 'doc' do
+  version '0.1.0'
+  sha256 '05d75e18e61f513a456c177e86549f49b2324b7435e457d413c4408a448872db'
+
+  url 'https://github.com/freshbitsio/homebrew-doc/raw/master/bin/v0.1.0/doc'
+  name 'doc'
+  homepage 'https://github.com/freshbitsio/homebrew-doc'
+
+  app 'doc'
+end
+
